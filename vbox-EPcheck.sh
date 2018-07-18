9c31badcaf2fd8c412864f13db6d3c097ab3d394
Michal Muransky <michal.muransky@telekom.com>
2018-07-18 23:42:06 +0200

    Initial version - should work :-)

---
+++ vbox-EPcheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Script will allign the VBox Extension Pack with currently installed VBox version
+#Good idea to run it after VBox update ;-)
+
+VBOXVER=`vboxmanage --version | cut -f1 -d"r"`
+VBOXEXTPACKVER=`vboxmanage list extpacks | grep Version | awk '{print $2}'`
+
+if [ "$VBOXVER" != "$VBOXEXTPACKVER" ]; then
+	wget https://download.virtualbox.org/virtualbox/$VBOXVER/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack -P /tmp/
+        echo "y"| VBoxManage extpack install --replace /tmp/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack &&
+        rm /tmp/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack
+fi
