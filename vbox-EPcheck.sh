8dd702a026b013477c293e27d1f76945a699540d
Michal <mike.muransky@gmail.com>
2018-07-24 11:37:41 +0200

    correct indentation

--- vbox-EPcheck.sh
+++ vbox-EPcheck.sh
@@ -6,11 +6,11 @@ VBOXVER=`vboxmanage --version | cut -f1 -d"r"`
 VBOXEXTPACKVER=`vboxmanage list extpacks | grep Version | awk '{print $2}'`
 
 if [ "$VBOXVER" != "$VBOXEXTPACKVER" ]; then
-	 wget https://download.virtualbox.org/virtualbox/$VBOXVER/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack -P /tmp/
-   echo "y"| vboxmanage extpack install --replace /tmp/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack
-	 VBOXEXTPACKVER=`vboxmanage list extpacks | grep Version | awk '{print $2}'`
-	 if [ "$VBOXVER" == "$VBOXEXTPACKVER" ]; then
-		 logger "VirtualBox Extension Pack has been updated to version "$VBOXEXTPACKVER
-     rm /tmp/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack
-	 fi
+  wget https://download.virtualbox.org/virtualbox/$VBOXVER/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack -P /tmp/
+  echo "y"| vboxmanage extpack install --replace /tmp/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack
+  VBOXEXTPACKVER=`vboxmanage list extpacks | grep Version | awk '{print $2}'`
+  if [ "$VBOXVER" == "$VBOXEXTPACKVER" ]; then
+    logger "VirtualBox Extension Pack has been updated to version "$VBOXEXTPACKVER
+    rm /tmp/Oracle_VM_VirtualBox_Extension_Pack-$VBOXVER.vbox-extpack
+  fi
 fi
